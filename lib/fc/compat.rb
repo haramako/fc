5e04da5a70869c2f37f90a5186f2b39efefab1c7
HARADA Makoto <haramako@gmail.com>
2014-03-21 15:37:57 +0900

    add compatibility for ruby 1.9.2.

--- lib/fc/compat.rb
+++ lib/fc/compat.rb
@@ -1,7 +1,19 @@
-# compatibility
+# backports for compatibility.
 
 unless defined? IO.write
   def IO.write(name, string)
-    File.open(name,'w'){|f| f.write string}
+    File.open(name,'w'){|f| f.write string }
+  end
+end
+
+unless defined? IO.binread
+  def IO.binread(name)
+    File.open(name,'rb:ASCII-8BIT'){|f| f.read }
+  end
+end
+
+unless defined? IO.binwrite
+  def IO.binwrite(name, string)
+    File.open(name,'wb:ASCII-8BIT'){|f| f.write string }
   end
 end
